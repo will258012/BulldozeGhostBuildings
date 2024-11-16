9ecc29061c0cfa69e0cd863cdb376f5565ec6f66
Will <125041335+will258012@users.noreply.github.com>
2024-11-16 21:36:07 +0800

    v1.0.1
    - 使用 WillCommons

--- BulldozeGhostLandfills/Mod.cs
+++ BulldozeGhostLandfills/Mod.cs
@@ -1,43 +1,10 @@
-﻿using BulldozeGhostLandfills.Utils;
-using ICities;
-using System.Reflection;
+﻿using WillCommons;
 namespace BulldozeGhostLandfills
 {
-    public class Mod : IUserMod, ILoadingExtension
+    public class Mod : PatcherModBase
     {
-        public string Name => "Bulldoze Ghost Landfills v" + ModVersion;
-        public string Description => "Use bulldozer to bulldoze your ghost landfills";
-        private const string HarmonyId = "Will258012.BulldozeGhostLandfills";
-
-        private string ModVersion
-        {
-            get
-            {
-                var assemblyVersion = ModAssembly.GetName().Version;
-                return $"{assemblyVersion.Major}.{assemblyVersion.Minor}.{assemblyVersion.Build}";
-            }
-        }
-        public void OnEnabled() => HarmonyPatcher.PatchOnReady(ModAssembly, HarmonyId);
-
-        public void OnCreated(ILoading loading)
-        {
-
-        }
-        public void OnLevelLoaded(LoadMode mode)
-        {
-
-        }
-
-        public void OnLevelUnloading()
-        {
-
-        }
-        public void OnReleased()
-        {
-        }
-
-        public void OnDisabled() => HarmonyPatcher.TryUnpatch(HarmonyId);
-        private Assembly ModAssembly => Assembly.GetExecutingAssembly();
-
+        public override string BaseName => "Bulldoze Ghost Landfills";
+        public override string Description => "Use bulldozer to bulldoze your ghost landfills";
+        public override string HarmonyID => "Will258012.BulldozeGhostLandfills";
     }
 }
